bf240f2eb4356094c5989582e36bba5169367ee6
Saad Srabon <saadsrabon2@gmail.com>
2024-01-15 04:19:50 +0600

    2024-01-15 04:19:50 - Hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
+import { Footer } from "./shared/Footer";
+import Header from "./shared/Header";
 
 
 const App = () => {
   return (
     <div>
 
+      <Header />
 
-      <section className="bg-[radial-gradient(50%_50%_at_50%_50%,#17956D_0%,#0F684C_100%)] pt-32 pb-20 -mt-[92px] md:-mt-[118px]">
-        <div className="container">
-          <div className="grid md:grid-cols-2 items-center w-10/12 mx-auto">
-            <img
-              className="md:order-2 object-cover ml-auto animate-updown"
-              src="./assets/hero-graphics.svg"
-              width="500px"
-              height="500px"
-              alt="Banner"
-            />
-            <div>
-              <h1 className="text-4xl lg:text-[56px] font-bold leading-[1.1] mb-8">
-                The Future of Learning starts with students at the center
-              </h1>
-              <a className="px-5 py-2.5 bg-[#038C61] rounded-[44px]" href="#">
-                Learn More
-              </a>
-            </div>
-          </div>
-        </div>
-      </section>
 
       <section className="py-24 lg:pt-[120px] lg:pb-28">
         <div className="container">
@@ -210,7 +192,7 @@ const App = () => {
         </div>
       </section>
 
-
+      <Footer />
     </div>
   );
 };
