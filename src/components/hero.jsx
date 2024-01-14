943b53dab5e294c5ae1af0c72392652ba89b29a5
Saad Srabon <saadsrabon2@gmail.com>
2024-01-15 04:21:26 +0600

    2024-01-15 04:21:26 - Component reorganized

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,6 +1,6 @@
 
 
-export const hero = () => {
+export const Hero = () => {
     return (
         <section className="bg-[radial-gradient(50%_50%_at_50%_50%,#17956D_0%,#0F684C_100%)] pt-32 pb-20 -mt-[92px] md:-mt-[118px]">
             <div className="container">
