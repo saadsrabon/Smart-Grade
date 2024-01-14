7125079aa8dfddf70a2920482976b8e20dab897c
Saad Srabon <saadsrabon2@gmail.com>
2024-01-15 04:16:45 +0600

    2024-01-15 04:16:45 - Header ready

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,7 @@
 const App = () => {
   return (
     <div>
-      <nav className="py-6">
-        <div className="container mx-auto flex items-center justify-between gap-x-6">
-          <a href="/">
-            <img
-              className="h-[40px]"
-              src="./assets/lws-logo-en.svg"
-              alt="Lws"
-            />
-          </a>
-          <a className="px-5 py-2 bg-[#172227] rounded-[44px]" href="#">
-            Get Admission
-          </a>
-        </div>
-      </nav>
+
 
       <section className="bg-[radial-gradient(50%_50%_at_50%_50%,#17956D_0%,#0F684C_100%)] pt-32 pb-20 -mt-[92px] md:-mt-[118px]">
         <div className="container">
--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -2,7 +2,20 @@
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <nav className="py-6">
+            <div className="container mx-auto flex items-center justify-between gap-x-6">
+                <a href="/">
+                    <img
+                        className="h-[40px]"
+                        src="./assets/lws-logo-en.svg"
+                        alt="Lws"
+                    />
+                </a>
+                <a className="px-5 py-2 bg-[#172227] rounded-[44px]" href="#">
+                    Get Admission
+                </a>
+            </div>
+        </nav>
     )
 }
 
